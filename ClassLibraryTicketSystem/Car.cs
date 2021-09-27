24ad49d519eb95d0c47b03f6a44ff52a0fd07c66
David Zikla <davi048b@edu.easj.dk>
2021-09-27 17:16:47 +0200

    T 1,2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    class Car
+    {
+        private string _licensePlate;
+        private DateTime _date;
+
+        public Car(string licensePlate, DateTime date)
+        {
+            _licensePlate = licensePlate;
+            _date = date;
+        }
+
+        public string LicensePlate { get => _licensePlate; set => _licensePlate = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+
+        public double Price()
+        {
+            return 240;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    class MC
+    {
+        private string _licensePlate;
+        private DateTime _date;
+
+        public MC(string licensePlate, DateTime date)
+        {
+            _licensePlate = licensePlate;
+            _date = date;
+        }
+
+        public string LicensePlate { get => _licensePlate; set => _licensePlate = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+
+        public double Price()
+        {
+            return 125;
+        }
+
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
